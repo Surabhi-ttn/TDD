078ab7bfcbfe40725b7bb53e6152edec5c11d8be
surabhi-ttn <surabhi.chaurasia@tothenew.com>
2021-05-15 22:45:37 +0530

    Initial Commit

---
+++ test/validator.js
@@ -0,0 +1,27 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const validator = require('../src/validator')
+
+describe("validator isNumValid()", () => {
+
+	it("should return true for a number in between 10 and 70", ()=> {
+		expect(validator.isNumValid(39)).to.be.true
+	})
+
+	it("should return false when the number is less than or equal to 10", () => {
+		expect(validator.isNumValid(10)).to.be.false
+	})
+
+	it("should return false when the number is greater than or equal to 70", () => {
+		expect(validator.isNumValid(79)).to.be.false
+	})
+
+    it("should return false when the number is equal to 70", () => {
+		expect(validator.isNumValid(70)).to.be.false
+	})
+
+	it("should return false when the number is greater than 70", () => {
+		expect(validator.isNumValid(71)).to.be.false
+	})
+})
